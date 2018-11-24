0ed65392a9b48199b63828a56ed9eb4d4ab88b5b
shubhang <shubhang@umich.edu>
2018-11-24 10:00:04 -0600

    Reaching end of minimax

--- TicTacToeAI.py
+++ TicTacToeAI.py
@@ -1,7 +1,8 @@
 import math
 
 game=["","","","","","","","",""]
-
+hu_player="X"
+ai_player="O"
 def game_status(game):
     count=0
     for i in range (9):
@@ -31,7 +32,14 @@ def game_status(game):
 
     return 0
 
-def wining (game,player)
+def wining (game,player):  
+
+    if (game[0]==player and game[4]==player and game[8]==player) or (game[2] == player and game[4] == player and game[6] == player) or (game[0] == player and game[1] == player and game[2] == player) or  (game[3] == player and game[4] == player and game[5] == player) or (game[6] == player and game[7] == player and game[8] == player) or (game[0] == player and game[3] == player and game[6] == player) or (game[1] == player and game[4] == player and game[7] == player) or (game[3]==player and game[5]==player and game[8]==player):
+        return True
+    else: 
+        return False  
+    
+
 def empty_indices(game):
     L=[]
     for i in inage (0,len(game)):
@@ -39,8 +47,43 @@ def empty_indices(game):
             L.append(i)
 
     return L
-def minimax(game, plauer):
+def minimax(game_in, player):
+    game=game_in
     avail_spot=empty_indices(game)
+    score=0
+    if winning(game,hu_player):
+        score=-10
+        return score
+
+    elif winning(game,ai_player):
+        score=10
+        return score
+    elif len(avail_spot)==0:
+        return score
+    
+
+    moves=[]
+
+    for i in range (0,len(avail_spot)):
+        move={}
+        move[i]=game[avail_spot[i]]
+
+        game[avail_spot[i]]=player
+
+        if (player==ai_player):
+            result =minimax(game.hu_player)
+            move.score = result[score]
+
+        game[avail_spot[i]]=move[i]
+
+        moves.append(move)
+
+
+    best_move=int
+
+    if player==ai_player:
+        
+        
 
 def print_board(game):
     print game[0]," | ",game[1]," | ",game[2]
@@ -52,8 +95,6 @@ def print_board(game):
 
 def main(game):
     print_board(game)
-    hu_player="X"
-    ai_player="O"
     while game_status(game)==0:
         inp=input("Enter the position you want to enter in(1-9): ")
         game[inp-1]= hu_player
